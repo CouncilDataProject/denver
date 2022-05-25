01e8b1bdafacc14eef206b0d3dc3238562d80fa9
CouncilDataProjectServiceAccount <councildataproject@gmail.com>
2022-05-25 19:43:45 +0000

    Initial commit

---
+++ web/src/index.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { App, AppConfigProvider } from "@councildataproject/cdp-frontend";
+
+import "@councildataproject/cdp-frontend/dist/index.css";
+
+const config = {
+    firebaseConfig: {
+        options: {
+            projectId: "cdp-denver-962aefef",
+        },
+        settings: {},
+    },
+    municipality: {
+        name: "Denver",
+        footerLinksSections: [],
+    },
+}
+
+ReactDOM.render(
+    <div>
+        <AppConfigProvider appConfig={config}>
+            <App />
+        </AppConfigProvider>
+    </div>,
+    document.getElementById("root")
+);
