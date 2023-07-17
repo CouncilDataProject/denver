09830a4980f1ee44d6f3c2d48ed63341f7d12b8f
CouncilDataProjectServiceAccount <CouncilDataProjectServiceAccount@users.noreply.github.com>
2023-07-17 23:33:13 +0000

    Deploying to gh-pages from @ CouncilDataProject/denver@63805a27f220d5f46126e82e124c69d4ee71445f 🚀

--- static/js/main.b928db92.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpcdp-denver"]=this["webpackJsonpcdp-denver"]||[]).push([[0],{207:function(e,n){},92:function(e,n,i){"use strict";i.r(n);i(0);var t=i(26),r=i.n(t),c=i(67),o=(i(221),i(59));r.a.render(Object(o.jsx)("div",{children:Object(o.jsx)(c.AppConfigProvider,{appConfig:{firebaseConfig:{options:{projectId:"cdp-denver-962aefef"},settings:{}},municipality:{name:"Denver",timeZone:"America/Denver",footerLinksSections:[]},features:{}},children:Object(o.jsx)(c.App,{})})}),document.getElementById("root"))}},[[92,1,2]]]);
-//# sourceMappingURL=main.b928db92.chunk.js.map
